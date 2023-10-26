31e9ee03c3189f1dafe3bcabc795fed950552967
Suhong Qin <suhongq@google.com>
2023-10-26 16:30:02 -0700

    action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,16 @@
 #!/bin/sh -l
 
-mkdir -p /github/workspace/artifacts
-
 cd /scancode-toolkit
-# ./scancode \
-# 	-clipeu \
-# 	--license --license-policy --license-text \
-# 	--classify \
-# 	--summary \
-# 	--verbose /github/workspace/$1 \
-# 	--processes `expr $(nproc --all) - 1` \
-# 	--json /github/workspace/artifacts/scancode.json \
-# 	--html /github/workspace/artifacts/scancode.html
 
-# ./scancode \
-# 	--license --package --copyright \
-# 	--license-score=70 \
-# 	--verbose /github/workspace/$1 \
-# 	--json /github/workspace/artifacts/scancode.json \
-# 	--csv /github/workspace/artifacts/scancode.csv
-
-./scancode /github/workspace/$1 \
-	--json /github/workspace/artifacts/scancode.json \
-	--csv /github/workspace/artifacts/scancode.csv \
+./scancode $1 \
+	--json scancode.json \
+	--csv scancode.csv \
   $2
 
 pwd .
-ls /github/workspace
+ls -al
 
-echo "json=artifacts/scancode.json" >> $GITHUB_OUTPUT
-echo "csv=artifacts/scancode.csv" >> $GITHUB_OUTPUT
+echo "json=scancode.json" >> $GITHUB_OUTPUT
+echo "csv=scancode.csv" >> $GITHUB_OUTPUT
 
 # python /license_check.py -c /github/workspace/license_config.yml -s /github/workspace/artifacts/scancode.json  -f /github/workspace/$1 -o /github/workspace/artifacts/report.txt
