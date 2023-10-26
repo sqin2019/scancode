b1204f3311e36d3b9bb6bb720a50c11acf9d1ccd
Suhong Qin <suhongq@google.com>
2023-10-26 15:59:52 -0700

    action

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,4 +25,7 @@ cd /scancode-toolkit
 	--csv /github/workspace/artifacts/scancode.csv \
   $2
 
+echo "json=artifacts/scancode.json" >> $GITHUB_OUTPUT
+echo "csv=artifacts/scancode.csv" >> $GITHUB_OUTPUT
+
 # python /license_check.py -c /github/workspace/license_config.yml -s /github/workspace/artifacts/scancode.json  -f /github/workspace/$1 -o /github/workspace/artifacts/report.txt
