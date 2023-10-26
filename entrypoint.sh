c81cea6b1435554441cff37d436578253c0cff36
Suhong Qin <suhongq@google.com>
2023-10-26 16:40:03 -0700

    action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 cd /scancode-toolkit
 
-./scancode $1 \
-	--json scancode.json \
-	--csv scancode.csv \
+./scancode /github/workflow/$1 \
+	--json /github/workflow/scancode.json \
+	--csv /github/workflow/scancode.csv \
   $2
 
 pwd .
