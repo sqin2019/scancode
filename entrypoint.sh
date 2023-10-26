309b37ff334b0182d3c4ae9402da9bb77920a5ce
Suhong Qin <suhongq@google.com>
2023-10-26 13:54:06 -0700

    action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,22 @@
-#!/bin/bash
-
-mkdir -p /github/workspace/artifacts
+# mkdir -p /github/workspace/artifacts
 
 cd /scancode-toolkit
+# ./scancode \
+# 	-clipeu \
+# 	--license --license-policy --license-text \
+# 	--classify \
+# 	--summary \
+# 	--verbose /github/workspace/$1 \
+# 	--processes `expr $(nproc --all) - 1` \
+# 	--json /github/workspace/artifacts/scancode.json \
+# 	--html /github/workspace/artifacts/scancode.html
+
 ./scancode \
-	-clipeu \
-	--license --license-policy --license-text \
-	--classify \
-	--summary \
+	--license --package --copyright \
+	--license-score=70 \
 	--verbose /github/workspace/$1 \
-	--processes `expr $(nproc --all) - 1` \
 	--json /github/workspace/artifacts/scancode.json \
 	--csv /github/workspace/artifacts/scancode.csv
+
+
+# python /license_check.py -c /github/workspace/license_config.yml -s /github/workspace/artifacts/scancode.json  -f /github/workspace/$1 -o /github/workspace/artifacts/report.txt
