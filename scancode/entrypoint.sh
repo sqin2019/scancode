a7e5fd67e215b388f58a392637c5f8bf4d18a3b0
Suhong Qin <suhongq@google.com>
2023-10-26 22:00:24 -0700

    action

--- scancode/entrypoint.sh
+++ scancode/entrypoint.sh
@@ -1,3 +1,4 @@
+#!/bin/sh -l
 # Copyright 2023 The Authors (see AUTHORS file)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/bin/sh -l
 
 cd /scancode-toolkit
 
